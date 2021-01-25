c623647cc5414c939f46a077e070f9ccec996e44
Harshit Keshari <harshit107.in@gmail.com>
2021-01-26 00:25:12 +0530

    harshit-searh

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = new express.Router();
 
 
-router.get('/upload/app', async (req, res) => {
+router.get('/applist', async (req, res) => {
 
+    console.log('applist')
     const detail = {
          appName : "Uem Connect",
          companyName :"Harshit",
@@ -15,7 +16,6 @@ router.get('/upload/app', async (req, res) => {
          appUrl : "abc.com",
          appVersion : "1.2.2",
     }   
-
     try {
         const app = await new AppList(detail);
         await app.save()
@@ -27,6 +27,24 @@ router.get('/upload/app', async (req, res) => {
         )
     }
    
+})
+
+router.get('/applist/search',async(req, res) => {
+
+    const appName = req.query.app || '';
+    try {
+
+        const searchResult = await AppList.find({ appName : new RegExp(appName, "i") });
+        if (searchResult.length == 0)
+            return res.status(404).send({ error: "No app found" });
+        res.send({message : searchResult})
+        
+    } catch (error) {
+        console.log({ error })
+        res.status(400).send({error})
+    }
+
+
 })
 
 module.exports = router;
